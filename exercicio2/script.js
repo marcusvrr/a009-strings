90403db02ad86b5919f7654fa713005db02d40dc
marcusvrr <marcusvrr2@gmail.com>
2022-07-25 22:09:17 -0300

    exercicio 2 feito

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,9 @@
 const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+//a) Remova o excesso de espaços no final da string;
+const trimString = minhaString.trim();
+console.log(trimString);
+//b)exiba, em um console.log() a quantidade de caracteres da string, antes e depois da remoção dos espaços;
+console.log(minhaString.length)
+console.log(trimString.length)
+//c) Substitua os traços `________` por “sticioso”.
+console.log(trimString.replace("________","sticioso"))
